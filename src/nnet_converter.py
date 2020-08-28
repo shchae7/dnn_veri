fb2b338d4b226e729e0a4db2acef8346b54c57ae
shchae7 <seunghyunchae@chaeseunghyeons-MacBook-Pro.local>
2020-08-28 17:59:51 +0900

    relu added

--- src/nnet_converter.py
+++ src/nnet_converter.py
@@ -8,35 +8,51 @@ def nnet_converter_python(file_path, dest_path):
     # Get nnet info
     weights, biases, numLayers, layerSizes = readNNet(file_path)
 
-    # Total num of Variables
-    numNodes = 0
-    for i in range(0, numLayers + 1):
-        numNodes  += layerSizes[i]
-
     fol_file = open(dest_path, 'w')
     fol_file.write('from z3 import *\n\n')
 
+    # Total num of Vars
+    numNodes = 0
+    for i in range(0, numLayers + 1):
+        numNodes += layerSizes[i]
+
+    # Var Declartation
     for i in range(0, numNodes):
         fol_file.write('x' + str(i) + ' = Real(\'x' + str(i) + '\')\n') # Python API: x0 = Reals('x0') SMT-LIB: (declare-const x0 Real)
     fol_file.write('\n')
 
     fol_file.write('s = Solver()\n\n')
 
-    test_file = open('test.txt', 'w')
+    # Network Constraints
     passedNodes = layerSizes[0]
-    for i in range(1, numLayers + 1):
+    for i in range(1, numLayers + 1): # Output layer nodes labeled x... as well
         for j in range(0, layerSizes[i]):
-            eq = 'x' + str(passedNodes + j) + ' == '
+            eq = ''
             for k in range(0, layerSizes[i - 1]):
-                if k == layerSizes[i - 1] - 1:
-                    eq += (str(weights[i - 1][j][k]) + ' * x' + str(passedNodes - layerSizes[i - 1] + k))
-                else:
-                    eq += (str(weights[i - 1][j][k]) + ' * x' + str(passedNodes - layerSizes[i - 1] + k) + ' + ')
-            print(eq + '\n', file = test_file)
+                eq += (str(weights[i - 1][j][k]) + ' * x' + str(passedNodes - layerSizes[i - 1] + k) + ' + ')   # Add weights * previous layer nodes
+            eq += (str(biases[i - 1][j]))        # Add bias
+
+            # ReLU activation function -->  (zi >= 0 /\ xi == zi) \/ (zi < 0 /\ xi == 0) /\ (zi == axi-1 + bxi-2 ...)
+            index = str(passedNodes + j)
+            fol_file.write('s.add(z' + index + ' == ' + eq + ')\n')
+
+            leftconstraint = 'x' + index + '_left = And([z' + index + ' >= 0, x' + index + ' == z' + index + '])'
+            fol_file.write(leftconstraint + '\n')
+
+            rightconstraint = 'x' + index + '_right = And([z' + index + ' < 0, x' + index + ' == 0])'
+            fol_file.write(rightconstraint + '\n')
+
+            reluconstraint = 'Or([x' + index + '_left, x' + index + '_right])'
+            fol_file.write('s.add(' + reluconstraint + ')\n\n')
+
         passedNodes += layerSizes[i - 1]
 
-    fol_file.write('s.add()\n')
+    # Input Constraints
 
+    # Property Constraints
+
+
+    fol_file.write('\n')
     fol_file.write('print(s.check())\n')
     fol_file.write('print(s.model())\n')
 
