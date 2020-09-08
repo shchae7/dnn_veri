91060acc8e65d9336ff7538e5b81ca1a8de884bf
SeungHyun Chae <seunghyunchae@SeungHyuns-MacBook-Air.local>
2020-09-08 21:28:00 +0900

    formula builder init

---
+++ formula_builder.py
@@ -0,0 +1,16 @@
+class Z3File():
+    def __init__(self):
+        self.formulas = []
+
+    def add_formula(self, formula):
+        self.formulas.append(formula)
+
+    def 
+
+class Z3Formula():
+    def __init__(self):
+        self.
+
+
+    def encodeReLU(self):
+        
